8ff6777dea928fee2d094a3a4542fa2e529d2198
ncy1224 <985767498@qq.com>
2023-03-15 13:29:31 +0800

    nodemon

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 const Koa = require("koa");
 
+const { APP_PORT } = require("./config/config.default")
+
 const app = new Koa();
 
-app.use((ctx, next)=> {
-    ctx.body="hello world"
-})
-
-app.listen("8080", ()=>{
-    console.log("listening port 8080");
+app.listen(APP_PORT, ()=>{
+    console.log(`listening port ${APP_PORT}`);
 })
